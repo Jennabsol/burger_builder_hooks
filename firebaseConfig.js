a6cc1389939926f9e7fa573cc03ce158b134e7e5
Jenna Solis <jennabsol@gmail.com>
2020-04-19 21:36:18 -0500

    finished auth

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyClvOZyyJqRPhkwz2nfAfgHB801CuHvZW4",
+    authDomain: "burgerbuilder-82510.firebaseapp.com",
+    databaseURL: "https://burgerbuilder-82510.firebaseio.com",
+    projectId: "burgerbuilder-82510",
+    storageBucket: "burgerbuilder-82510.appspot.com",
+    messagingSenderId: "200875229795",
+    appId: "1:200875229795:web:f627494a5525a655a26e56",
+    measurementId: "G-XPHMM90DJC"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
