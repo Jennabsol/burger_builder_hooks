eaac100463b1ef6efce0175ecbd1f5d71bb7d09e
Jenna Solis <jennabsol@gmail.com>
2020-04-20 01:26:26 -0500

    added custom hook

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ const Auth = React.lazy(() => {
 });
 
 const app = (props) => {
+
+	const {onTryAutoSignup} = props
 	
 	useEffect(() => {
-		props.onTryAutoSignup();
-	}, []);
+		onTryAutoSignup();
+	}, [onTryAutoSignup]);
 
 	let routes = (
 		<Switch>
